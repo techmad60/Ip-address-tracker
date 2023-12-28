af81406c540fdb0199cc876186dae57373df1890
Success Obasi <obasisuccess192@gmail.com>
2023-12-28 16:55:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-blue-400'>Ip address tracker hi</h1>
+        <p className='text-blue-600'>Welcome back  bro</p>
+      </div>
+    </>
+  )
+}
+
+export default App
